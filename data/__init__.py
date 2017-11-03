06370dc2bd7531e536daf905e4d6b6725658baf5
bitgardener <bviehhauser@yahoo.de>
2017-11-03 11:22:20 +0100

    added the MNIST dataset of hand written digits. __init__.py converts this data to numpy arrays for training

---
+++ data/__init__.py
@@ -0,0 +1,26 @@
+# unzips and converts data for neural net
+import gzip
+import os.path as p
+import numpy as np
+
+
+# startIndex sets how many values should be skipped
+# in the raw byte stream
+# ( skipps metadata )
+def unzipData(filename, startIndex):
+    with gzip.open(filename) as file:
+        data = file.read()
+
+        return list(data[startIndex:])
+
+
+dir = p.dirname(__file__)
+x_train = unzipData(p.join(dir, ".\\train-images-idx3-ubyte.gz"), 16)
+y_train = unzipData(p.join(dir, ".\\train-labels-idx1-ubyte.gz"), 8)
+x_test  = unzipData(p.join(dir, ".\\t10k-images-idx3-ubyte.gz"), 16)
+y_test  = unzipData(p.join(dir, ".\\t10k-labels-idx1-ubyte.gz"), 8)
+
+x_train = np.array(x_train).reshape((60000, 28 * 28))
+y_train = np.array(y_train).reshape((60000, 1))
+x_test  = np.array(x_test).reshape((10000, 28 * 28))
+y_test  = np.array(y_test).reshape((10000, 1))
